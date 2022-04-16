1ceb4c6f03d6cc8a88d926ba76d34afef78cdbac
Josiah-Canada <josiahcanada@yahoo.com>
2022-04-16 12:03:43 -0400

    added times and colors

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+//var row1 = document.getElementById("9am")
+
+var currentTime = new Date().getHours();
+console.log(currentTime)
+// var currentHour = document.getElementById(currentTime)
+// currentHour.setAttribute("class", "row time-block present")
+
+for (var i = 9; i <= 17; i++) {
+    var currentHour = document.getElementById(i)
+    if (i==currentTime) {
+        currentHour.setAttribute("class", "row time-block present")
+    } else if (i < currentTime) {
+        currentHour.setAttribute("class", "row time-block past")
+    } else {
+        currentHour.setAttribute("class", "row time-block future")
+    }
+}
+document.getElementById("11").getElementsByClassName("saveBtn")[0].addEventListener("click", function(){
+    console.log("hi")
+});
+console.log(document.getElementById("11").getElementsByClassName("saveBtn")[0])
 
